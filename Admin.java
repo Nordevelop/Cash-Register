91bef6a2a5340a7ab3491c4e35dbefac4522e931
Nordevelop <88049567+Nordevelop@users.noreply.github.com>
2021-11-27 22:42:45 -0300

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -1,1 +1,12 @@
+#Java
+import java.utils.*;
 
+public class Admin{
+  /*Project cash-Register
+  *Author  Nordevelop
+  */
+  /*Class Admin for load the data entered by keyboard.-
+  *  into the structure of an Arraylist.
+  */
+  
+}

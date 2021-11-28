3cf01ccd0240f5ee9215242333e616995a531748
Nordevelop <88049567+Nordevelop@users.noreply.github.com>
2021-11-27 22:09:43 -0300

    Update Deliver.java

--- Deliver.java
+++ Deliver.java
@@ -1,1 +1,38 @@
+import java.util.*;
 
+public class Deliver {
+    /*
+    *objects of class Deliver emcapsulated.with Access Denied in class Main()
+    *@Author Nordevelop
+    *Cash Register
+    */ 
+    
+	private int id;              
+	private String description;
+	private int prize;
+	private int quantity;
+	private int subtotal;
+  //private String nombre_cliente;
+  
+	
+	
+	public int darId()
+	{
+		return this.id;
+	}
+	public String darDescripcion()  //metodo 
+	{
+		return this.descripcion;
+	}
+	public int darPrecio()
+	{
+		return this.precio;
+		
+	}
+	public int darCantidad()
+	{
+		return this.cantidad;
+	}
+	public int darSubtotal()
+	{
+		return 

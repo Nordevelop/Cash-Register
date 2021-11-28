79a13ff1da6214fce8c91fe873281db5830aaecb
Nordevelop <88049567+Nordevelop@users.noreply.github.com>
2021-11-27 21:51:53 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,30 @@
+import java.util.InputMismatchException; 
+/*
+ *  exception to add numbers as char
+ */
 
+import java.util.*;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		/*
+		 * author Nordevelop
+		 * program Cash register
+		 * class Main, deliver,admin
+		 */
+		Scanner enter = new Scanner (System.in); //capture buffer of keyboard
+		Deliver cla= null; //Class Deliver
+    int id;
+		String description;
+		int prize=0;
+		int quantity = 0;
+		int subtotal=0;
+		int total=0;
+    ArrayList<Deliver> List = new ArrayList<Deliver>();
+    System.out.println("Enter an ID or zero to end: ");
+		   id=enter.nextInt();
+		   ArrayList<Registro> List = null;
+   
+	}
